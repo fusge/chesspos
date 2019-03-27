03eec258194bc06bcc56015ba6a5b82962da10a1
Ivan Valerio <valerioif@gmail.com>
2019-03-26 23:33:28 -0400

    Changed dir name for static files and created a generic html template

--- chesspossite/chesspossite/settings.py
+++ chesspossite/chesspossite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ez81@&4vlnuv!-+a=vvqvem(-nr=r2-uw72^u)!xlkgrbl&q#9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'chesspossite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
