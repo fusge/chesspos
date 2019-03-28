a8ad72cf5f66be3c797c2d7df38df0249d8081cd
Ivan Valerio <valerioif@gmail.com>
2019-03-27 21:54:28 -0400

    built the navigation bar. pretty tough at first

--- chesspossite/chessboard/views.py
+++ chesspossite/chessboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'chessboard/chessboard.html')
+    return render(request, 'chessboard/chessboard.html', {'title': 'board'})
 
 def about(request):
     return render(request, 'chessboard/about.html', {'title': 'About'})
--- chesspossite/chesspossite/views.py
+++ chesspossite/chesspossite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('This is the main page of the site. Add content here!')
+    return render(request, 'chesspossite/home.html', {'title': 'Home'})
 
 def about(request):
-    return HttpResponse('This is the about page of the site.')
+    return render(request, 'chesspossite/about.html', {'title': 'About'})
