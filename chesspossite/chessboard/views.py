e76d71c4583650f0db1b2b89cc5804f42a361063
Ivan F. Valerio <valerioif@gmail.com>
2023-02-16 20:37:35 -0500

    updating project for modern django

--- chesspossite/chessboard/views.py
+++ chesspossite/chessboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'chessboard/chessboard.html', {'title': 'board'})
+
 
 def about(request):
     return render(request, 'chessboard/about.html', {'title': 'About'})
